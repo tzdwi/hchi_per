50c46059ab0280cb1ef450f5a7146a451530647a
Trevor Dorn-Wallenstein <trevorzaylen@gmail.com>
2018-01-14 19:53:45 -0800

    fixed sorting regions at the last minute because i fucked up

--- info/script_arctic.py
+++ info/script_arctic.py
@@ -5,9 +5,10 @@ from sys import argv
 """
 Author: Trevor Dorn-Wallenstein
 Date: 12/21/17
-Purpose: Call python script_arctic.py region_file filt1 filt2 exp1 exp2 to generate the
+Purpose: Call python script_arctic.py region_file filt1 filt2 exp1 exp2 root1 root2 to generate the
 ARCTIC+APO low-level scripting commands that will tile the locations given in region_file,
 using the exposure times given in exp1 and exp2 to expose in filt1 and filt2 respectively.
+If called with the final parameter n_start
 """
 
 if __name__ == '__main__':
@@ -19,6 +20,17 @@ if __name__ == '__main__':
     
     exp1 = float(argv[4])
     exp2 = float(argv[5])
+    
+    root1 = str(argv[6])
+    root2 = str(argv[7])
+    
+    if len(argv) > 8:
+        
+        n_start = int(argv[8])
+    
+    else:
+        
+        n_start = 1
     
     with open(region_file,'r') as f:
         reg_string = f.read().split('\n')[3:]
@@ -35,12 +47,17 @@ if __name__ == '__main__':
         
     out_str = ''
     
-    for ra,dec in zip(ras,decs):
-        out_str += 'MOVE {0} {1}\n'.format(ra,dec)
-        out_str += 'arctic set filter={}\n'.format(filt1)
-        out_str += 'arcticExpose object time={}\n'.format(exp1)
-        out_str += 'arctic set filter={}\n'.format(filt2)
-        out_str += 'arcticExpose object time={}\n'.format(exp2)
+    ns = np.arange(1,len(ras)+1)
+    
+    for ra,dec,n in zip(ras,decs,ns):
+        
+        if n >= n_start:
+            
+            out_str += 'MOVE {0} {1}\n'.format(ra,dec)
+            out_str += 'arctic set filter={}\n'.format(filt1)
+            out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp1,root1,n)
+            out_str += 'arctic set filter={}\n'.format(filt2)
+            out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp2,root2,n)
         
     with open('arctic_script.txt','w') as f:
         f.write(out_str)
