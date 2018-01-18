386d6ceadac7a3c79069b3c9208be066bc82fbdc
Trevor Dorn-Wallenstein <trevorzaylen@gmail.com>
2018-01-18 10:36:02 -0800

    finished observing run, data in .gitignore

--- info/script_arctic.py
+++ info/script_arctic.py
@@ -5,10 +5,12 @@ from sys import argv
 """
 Author: Trevor Dorn-Wallenstein
 Date: 12/21/17
-Purpose: Call python script_arctic.py region_file filt1 filt2 exp1 exp2 root1 root2 to generate the
-ARCTIC+APO low-level scripting commands that will tile the locations given in region_file,
-using the exposure times given in exp1 and exp2 to expose in filt1 and filt2 respectively.
-If called with the final parameter n_start
+Purpose: Call python script_arctic.py region_file filt1 filt2 exp1 exp2 root1 root2 n_per_filt 
+to generate the ARCTIC+APO low-level scripting commands that will tile the locations given in 
+region_file, using the exposure times given in exp1 and exp2 to expose in filt1 and filt2 respectively.
+File names are root1_nfield root2_nfield. n_per_filt is the number of pointings to observe before 
+doing a filter change.
+If called with the final parameter n_start, will start with the n_start'th field
 """
 
 if __name__ == '__main__':
@@ -24,20 +26,24 @@ if __name__ == '__main__':
     root1 = str(argv[6])
     root2 = str(argv[7])
     
-    if len(argv) > 8:
+    n_per_filt = int(argv[8])
+    
+    if len(argv) > 9:
         
-        n_start = int(argv[8])
+        n_start = int(argv[9])
     
     else:
         
         n_start = 1
     
+    #pull out the strings for the region files of the field
     with open(region_file,'r') as f:
         reg_string = f.read().split('\n')[3:]
         
     ras = []
     decs = []
     
+    #Do some parsing to split out the string into ra, dec
     for pos in reg_string:
         
         split = re.split(',|\(',pos)
@@ -47,17 +53,51 @@ if __name__ == '__main__':
         
     out_str = ''
     
-    ns = np.arange(1,len(ras)+1)
+    n_track = n_start
     
-    for ra,dec,n in zip(ras,decs,ns):
+    while n_track < len(ras)+1-n_per_filt:
         
-        if n >= n_start:
+        out_str += 'arctic set filter={}\n'.format(filt1)
+    
+        for i in range(n_track,n_track+n_per_filt):
             
-            out_str += 'MOVE {0} {1}\n'.format(ra,dec)
-            out_str += 'arctic set filter={}\n'.format(filt1)
-            out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp1,root1,n)
-            out_str += 'arctic set filter={}\n'.format(filt2)
-            out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp2,root2,n)
+            ra = ras[i-1]
+            dec = decs[i-1]
+            
+            out_str += 'tcc track {0}, {1}\n'.format(ra,dec)
+            out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp1,root1,i)
+            
+        out_str += 'arctic set filter={}\n'.format(filt2)
         
+        for i in range(n_track,n_track+n_per_filt):
+            
+            ra = ras[i-1]
+            dec = decs[i-1]
+            
+            out_str += 'tcc track {0}, {1}\n'.format(ra,dec)
+            out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp2,root2,i)
+            
+        n_track += n_per_filt
+        
+    out_str += 'arctic set filter={}\n'.format(filt1)
+    
+    for i in range(n_track,len(ras)+1):
+            
+        ra = ras[i-1]
+        dec = decs[i-1]
+
+        out_str += 'tcc track {0}, {1}\n'.format(ra,dec)
+        out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp1,root1,i)
+        
+    out_str += 'arctic set filter={}\n'.format(filt2)
+    
+    for i in range(n_track,len(ras)+1):
+            
+        ra = ras[i-1]
+        dec = decs[i-1]
+
+        out_str += 'tcc track {0}, {1}\n'.format(ra,dec)
+        out_str += 'arcticExpose object time={0} name={1}_{2}\n'.format(exp2,root2,i)
+
     with open('arctic_script.txt','w') as f:
         f.write(out_str)
